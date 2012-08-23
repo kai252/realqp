e140067fa5f2b883c4c51f54303278aa44e2e31f
Phillip <kai252@gmail.com>
2012-08-23 12:36:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realqp::Application.config.secret_token = 'dbe58b52b75e84011f80b90c660858bd0df6c2656b28b94c54ac83a82c2276e340425ea56c59ed3fa4d5d6fbe755aa5072d835fbcfa0a752da4c2c0850629b59'
